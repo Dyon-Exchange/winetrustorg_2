addef3bf3349d084996b442d4ea52b68aef934a8
virlanchainworks <virlanchainworks@gmail.com>
2022-02-03 07:17:45 +0800

    staging

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 function getProductByName() {
     // const productURL = "http://127.0.0.1:3030/public/assets/product/";
-    const productURL = "https://dev.winetrust.org/db/public/assets/product/"
+    // const productURL = "https://dev.winetrust.org/db/public/assets/product/"
+    const productURL = "https://staging.winetrust.org/db/public/assets/product/"
     let stringQuery = document.getElementById("productsearchbyname").value;
     console.log(`${productURL}${stringQuery}`)
     fetch(`${productURL}${stringQuery}`)
@@ -29,7 +30,8 @@ function getProductByName() {
 
 function getAssetByTokenId() {
     // const productURL = "http://127.0.0.1:3030/public/assets/token/";
-    const productURL = "https://dev.winetrust.org/db/public/assets/token/"
+    // const productURL = "https://dev.winetrust.org/db/public/assets/token/"
+    const productURL = "https://staging.winetrust.org/db/public/assets/product/"
     let stringQuery = document.getElementById("assetsearchbyid").value;
     fetch(`${productURL}${stringQuery}`)
     .then(data => {
